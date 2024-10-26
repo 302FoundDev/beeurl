6b869bed4feb3f9be883d640f444b15950b4b677
4quacode <jarscode.dev@outlook.com>
2024-10-26 07:44:59 +0200

    Change background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import { ProtectedRoute } from './middleware/protectedRoutes'
 const App = () => {
   return (
     <main className='max-w-screen min-h-screen'>
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-black bg-custom-gradient bg-custom-size"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <Navbar />
       <Routes>
         // Public routes
